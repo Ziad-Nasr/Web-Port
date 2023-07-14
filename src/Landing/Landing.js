392543870627a8808f2cc195a1425d9bc318cfb1
ZiadNasr <ziadnasr988@gmail.com>
2023-07-14 21:29:23 +0300

    Starting Porjects + Adjusting Headlines

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Landing.css";
+import Project from "../Projects/Projects";
 
 function Landing() {
     return (
         <div className="body">
             <div className="Welcome">
                 <div className="WelcomeText">
-                    <h1 className="WelcomeTitle">Hello, I am Ziad Nasr,<br/>A Data Scientist</h1>
+                    <h1 className="WelcomeTitle">Hello, I am Ziad Nasr,<br/>A Software Engineer</h1>
                     <p className="Subtitle">Lorem Ipsum. Lorem Ipsum. Lorem Ipsum</p>
                 </div>
+            </div>
+            <div className="Projects">
+                <Project />
             </div>
         </div>
     )
---
+++ src/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Project.css';
+function Project(){
+    
+    return (
+        <div className="ProjectBody">
+            <div className="Title-SubTitle">
+            <h2>Projects Glimpse</h2>
+            <p>Sample lists of projects that i worked on/Contributed.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
